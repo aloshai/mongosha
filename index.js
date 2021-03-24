e90182e734ff4a8fe67b26f1d790336ce8400911
iiAlosha <arma.55@outlook.com>
2021-03-24 19:44:00 +0300

    I am a gai

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const mongosha = require("./src/index.js");
+const mongodb = require('mongodb');
+const { Client } = require('./src/client/Client.js');
 
-module.exports = mongosha;
+const { Collection } = require("./src/structers/Collection.js");
+const { Data } = require("./src/structers/Data.js");
+const { Database } = require("./src/structers/Database.js");
+
+class Mongosha {
+    /**
+     * Creates a new client and connects to MongoDB. (Connects to socket)
+     * @param {String} url 
+     * @param {mongodb.MongoClientOptions} options 
+     * @return {Promise<Client>}
+     */
+    static connect = async function (url, options = undefined) {
+        let mongoClient = await mongodb.connect(url, options);
+        let client = new Client(mongoClient);
+        
+        return client;
+    }
+}
+
+module.exports = { Mongosha, Collection, Data, Database, Client };
